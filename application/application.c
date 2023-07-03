21c377c340cee9c44fe89227b69f5b25ceb1074d
Gustavo Comarú <gustavo.comaru.rod@gmail.com>
2023-07-03 17:15:35 -0300

    WIP

--- application/application.c
+++ application/application.c
@@ -1,42 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MATRIX_SIZE         4
+
 #define CONV_IDLE           0
 #define CONV_REQUEST        1
 #define CONV_PROCESSING     2
 #define CONV_READY          4
-#define CONV_FINISH         8
 
 #define MMR_BASE ((unsigned int *)0x10000000)
 
-volatile int *conv_status  = MMR_BASE + 0; // ((unsigned int *)0x10000000)
-volatile int *conv_m_size  = MMR_BASE + 1; // ((unsigned int *)0x10000004)
-volatile int *conv_addr_m1 = MMR_BASE + 2; // ((unsigned int *)0x10000008)
-volatile int *conv_addr_m2 = MMR_BASE + 3; // ((unsigned int *)0x1000000C)
-volatile int *conv_addr_re = MMR_BASE + 4; // ((unsigned int *)0x10000010)
+volatile int            *conv_status  = MMR_BASE + 0; // ((unsigned int *)0x10000000)
+volatile int            *conv_m_size  = MMR_BASE + 1; // ((unsigned int *)0x10000004)
+volatile unsigned int   *conv_addr_m1 = MMR_BASE + 2; // ((unsigned int *)0x10000008)
+volatile unsigned int   *conv_addr_m2 = MMR_BASE + 3; // ((unsigned int *)0x1000000C)
+volatile unsigned int   *conv_addr_re = MMR_BASE + 4; // ((unsigned int *)0x10000010)
+
+int m1[MATRIX_SIZE][MATRIX_SIZE];
+int m2[MATRIX_SIZE][MATRIX_SIZE];
+int mr[MATRIX_SIZE][MATRIX_SIZE];
+
+void print_matrix(int *m, int size) {
+    printf("m_ptr = %p\n",  (void*)m);
+
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++) {
+            printf("%02d ", *(m + i * size + j));
+        }
+        printf("\n");
+    }
+}
 
 int main(int argc, char **argv) {
 
-    // First test
+    for(int i = 0; i < MATRIX_SIZE; i++) {
+        for(int j = 0; j < MATRIX_SIZE; j++) {
+            m1[i][j] = i + j;
+            m2[i][j] = (i + j) * 2;
+        }
+    }
 
-    int result = *conv_addr_re;
-    printf("Res 0: %d\n", result);
+    printf("Matrix 1:\n");
+    print_matrix(m1, MATRIX_SIZE);
+    printf("\n");
 
-    // Second test
+    printf("Matrix 2:\n");
+    print_matrix(m2, MATRIX_SIZE);
+    printf("\n");
 
-    int a = 361;
-    int b = 125;
+    while(*conv_status != CONV_IDLE);
 
-    *conv_addr_m1 = a;
-    *conv_addr_m2 = b;
-    *conv_status = CONV_REQUEST;
+    printf("m1_ptr = %p\n",  (void*)m1);
+    printf("m2_ptr = %p\n",  (void*)m2);
+    printf("res_ptr = %p\n", (void*)mr);
+    printf("size = %d\n", MATRIX_SIZE);
 
-    result = *conv_addr_re;
-    printf("Res 2: %u\n", result);
+    (*conv_addr_m1) = (unsigned int) m1;
+    (*conv_addr_m2) = (unsigned int) m2;
+    (*conv_addr_re) = (unsigned int) mr;
+    (*conv_m_size)  = MATRIX_SIZE;
+    (*conv_status)  = CONV_REQUEST;
 
-    // Finish
+    while(*conv_status != CONV_READY);
 
-    printf("\nHello first world from iGen generated platform\n\n");
+    *conv_status = CONV_IDLE;
+
+    printf("Resulting Matrix:\n");
+    print_matrix(mr, MATRIX_SIZE);
+    printf("\n");
 
     return 0;
 }
--- peripheral/convolution.c
+++ peripheral/convolution.c
@@ -9,6 +9,7 @@
 
 #include "convolution.igen.h"
 #include <string.h>
+#include <stdio.h>
 
 #define CONV_IDLE           0
 #define CONV_REQUEST        1
@@ -16,7 +17,85 @@
 #define CONV_READY          4
 #define CONV_FINISH         8
 
-unsigned int val = 20;
+// Global variable to handle a flit in VECTOR type
+char chFlit[4];
+
+// Global variable to handle a flit in UNSIGNED type
+unsigned int usFlit;
+
+/////////////////////////// Big endian/Little endian ///////////////////////////
+#define __bswap_constant_32(x) \
+     ((((x) & 0xff000000) >> 24) | (((x) & 0x00ff0000) >>  8) |		      \
+      (((x) & 0x0000ff00) <<  8) | (((x) & 0x000000ff) << 24))
+
+unsigned int htonl(unsigned int x){
+    return x;//__bswap_constant_32(x);
+}
+
+// Transform a flit from VECTOR to UNSIGNED INT (using the global variables)
+void vec2usi(){
+    unsigned int auxFlit = 0x00000000;
+    unsigned int aux;
+    aux = 0x000000FF & chFlit[3];
+    auxFlit = ((aux << 24) & 0xFF000000);
+    aux = 0x000000FF & chFlit[2];
+    auxFlit = auxFlit | ((aux << 16) & 0x00FF0000);
+    aux = 0x000000FF & chFlit[1];
+    auxFlit = auxFlit | ((aux << 8) & 0x0000FF00);
+    aux = 0x000000FF & chFlit[0];
+    auxFlit = auxFlit | ((aux) & 0x000000FF);
+    usFlit = auxFlit;
+    return;
+}
+
+// Writes a flit inside an address in the processor memory
+void writeMem(unsigned int flit, unsigned int addr){
+    usFlit = flit;
+    usi2vec();
+    ppmAddressSpaceHandle h = ppmOpenAddressSpace("MWRITE");
+    if(!h) {
+        bhmMessage("I", "NI_ITERATOR", "ERROR_WRITE h handling!");
+        while(1){} // error handling
+    }
+    ppmWriteAddressSpace(h, addr, sizeof(chFlit), chFlit);
+    ppmCloseAddressSpace(h);
+    return;
+}
+
+// Reads a flit from a given address
+unsigned int readMem(unsigned int addr){
+    ppmAddressSpaceHandle h = ppmOpenAddressSpace("MREAD");
+    if(!h) {
+        bhmMessage("I", "NI_ITERATOR", "ERROR_READ h handling!");
+        while(1){} // error handling
+    }
+    ppmReadAddressSpace(h, addr, sizeof(chFlit), chFlit);
+    ppmCloseAddressSpace(h);
+    vec2usi(); // transform the data from vector to a single unsigned int
+    return usFlit;
+}
+
+//////////////////////////////// Multiply Matrices ////////////////////////////////
+
+void multiply(volatile int *m1, int *m2, int *res, int size) {
+    
+    printf("m1_ptr = %p\n",  (void*)m1);
+    printf("m2_ptr = %p\n",  (void*)m2);
+    printf("res_ptr = %p\n", (void*)res);
+    printf("size = %d\n", size);
+    
+    printf("Conv:\n");
+    int i, j;
+    for(i = 0; i < size; i++) {
+        for(j = 0; j < size; j++) {
+
+            // ((int **)res)[i][j] = ((int **)m1)[i][j] + ((int **)m2)[i][j];
+            printf("%02d ", *(m1 + i * size + j));
+
+        }
+        printf("\n");
+    }
+}
 
 //////////////////////////////// Callback stubs ////////////////////////////////
 
@@ -108,8 +187,10 @@ PPM_REG_WRITE_CB(status_W) {
     }
     *(Uns32*)user = data;
     // YOUR CODE HERE (status_W)
+    printf("!!\n");
     if(convolutionPort_ab_data.status.value == CONV_REQUEST) {
-        convolutionPort_ab_data.addr_re.value = convolutionPort_ab_data.addr_m1.value + convolutionPort_ab_data.addr_m2.value;
+        printf("!!!!!\n");
+        multiply((int*)convolutionPort_ab_data.addr_m1.value, (int*)convolutionPort_ab_data.addr_m2.value, (int*)convolutionPort_ab_data.addr_re.value, convolutionPort_ab_data.m_size.value);
         convolutionPort_ab_data.status.value = CONV_READY;
     }
 }
